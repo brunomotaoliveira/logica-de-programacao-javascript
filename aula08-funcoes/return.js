7fe27bb8057fedc6cc2468ca19b81a95c66b5ad0
brunomotadev <brunomotadev@gmail.com>
2021-07-02 18:59:40 -0300

    funcoes, return, arrowfunctions

---
+++ aula08-funcoes/return.js
@@ -0,0 +1,11 @@
+function somar(a,b){
+    return a + b;
+}
+
+let resultado = somar(1,3);
+console.log(resultado);
+
+//mesma função, porem armazenando o valor na variavel
+const somarVariavel = function(a,b){return a+b;}
+
+console.log(somarVariavel(1,3));
---
+++ aula08-funcoes/script.js
@@ -0,0 +1,7 @@
+function escreva(texto){
+    console.log(texto);
+}
+
+escreva('meu primeiro parametro');
+
+escreva('meu segundo parametro');
