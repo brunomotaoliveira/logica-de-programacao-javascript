d9670273a3a016576d1eda1d843c33cbe4db1ce1
brunomotadev <brunomotadev@gmail.com>
2021-07-02 14:41:46 -0300

    variaveis em javascript

---
+++ aula04-variaveis/script.js
@@ -0,0 +1,16 @@
+let a = 10;
+let b = 13.5;
+
+let c = "Bruno"; //string, sequencia de caracteres
+
+let d = true; // booleano "true" ou "false"
+
+d = 15;
+
+a = function(){};
+
+let e = null;
+
+console.log(e);
+
+const pi = 3.14;
