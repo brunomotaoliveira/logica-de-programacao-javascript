43686fb1fb1c379cb75fbe8efa0c249b628b0192
brunomotadev <brunomotadev@gmail.com>
2021-07-02 17:50:31 -0300

    Vetores, Percorrendo Vetores e Matrizes for in e for of

---
+++ aula07-vetores/percorrendo_vetor_for_in.js
@@ -0,0 +1,5 @@
+let vetor = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+// percorre o indice do vetor, usado geralmente em vetores do mesmo tamamnho. Permitindo acessar o elemento, de ambos os vetores.
+for(let indice in vetor) {
+    console.log(vetor[indice]);
+}
---
+++ aula07-vetores/script.js
@@ -0,0 +1,3 @@
+let vetor = [10,20,30,40,50];
+vetor[2] = '30';
+console.log(vetor[2]);
